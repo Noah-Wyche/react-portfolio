d7a1a3e4da5e00dba7e0d92f60303c761f04bb8d
Noah Fleetwood <noahwyche1@gmail.com>
2024-08-13 16:05:09 -0400

    further resume section editing, added margins and split text between pictures

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -7,8 +7,9 @@ const Resume = () => {
   return (
     <div className="ResumeContainer">
       <h1 className="ResumeTitle">Resume</h1>
-      <p className="ResumeContent">My Proffesional Resume and my Coding Bootcamp Certication</p>
+      <p className="ResumeContent">My Proffesional Resume</p>
       <img src={Resumeimage} alt="Resume" className="resume-image" />
+      <p className="CertificateContent">My Coding Bootcamp Certificate</p>
       <img src={Certificateimage} alt="Coding Certificate" className="certificate-image" />
     </div>
   );
